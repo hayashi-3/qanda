16dcce87d91f1c60d703a0bbad5082df88015c5f
hayashi-3 <hayashi.gml@gmail.com>
2021-01-27 02:49:05 +0000

    qanda done

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Question.create(id: 1, name: "Test name 1", title: "Test question 1", content: "Test question 1")
+Question.create(id: 2, name: "Test name 2", title: "Test question 2", content: "Test question 2")
+Question.create(id: 3, name: "Test name 3", title: "Test question 3", content: "Test question 3")
